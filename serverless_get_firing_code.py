b851d72b0b5966ba9daada23bb5a9dc56c90c0fa
Justin Iravani <justin.iravani@1strategy.com>
2017-09-18 09:23:12 -0600

    Adding error handling to the firing_code.py

--- serverless_get_firing_code.py
+++ serverless_get_firing_code.py
@@ -3,8 +3,10 @@ import boto3
 sfn_client = boto3.client('stepfunctions')
 
 def lambda_handler(event, context):
-    
-    response = sfn_client.get_activity_task(activityArn=event['activity_arn'])
-    
-    event['firing_code'] = response['taskToken']
+
+    try:
+        response = sfn_client.get_activity_task(activityArn=event['activity_arn'])
+        event['firing_code'] = response['taskToken']
+    except Exception as error:
+        print(error)
     return event
