af3fc44b7a6c023dfadddf3b4c22a991bc92120d
jiravani <j.iravani@gmail.com>
2018-05-24 11:50:56 -0600

    Update lambda-function.py

--- lambda-function.py
+++ lambda-function.py
@@ -3,7 +3,7 @@ def lambda_handler(event, context):
   event['activity_arn'] = os.environ['activity_arn']
   event['sns_arn'] = os.environ['sns_arn']
   event['sqs_name'] = os.environ['sqs_name']
-  event['dynamodb_table'] = os.environ[‘dynamodb_table']
+  event['dynamodb_table'] = os.environ['dynamodb_table']
 
   event['commander'] = {
   "rank":"<REPLACE ME>",
