4620230f99b245dacc955b4d934041b77b025de5
jiravani <j.iravani@gmail.com>
2018-05-25 08:52:29 -0600

    Create curl-command.sh

---
+++ curl-command.sh
@@ -0,0 +1,3 @@
+curl -X POST \
+-d '{ "input": "{}", "stateMachineArn": "arn:aws:states:us-west-2:281782457076:stateMachine:your-name-state-machine"}' \
+https://00dlxb9yr1.execute-api.us-west-2.amazonaws.com/prod/startexecution/your-name
